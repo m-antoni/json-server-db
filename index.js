e093397047f0311bbff0a56f970f0eff20d82bf1
Michael Antoni <michaelantoni.cs@gmail.com>
2022-08-20 04:25:18 +0800

    update id and create custom api route

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3500;
+const port = process.env.PORT || 5000;
 
+// middleware
 server.use(middlewares);
-server.use(router);
+server.use('/api/v1',router);
 
 server.listen(port);
